f69b961436a907bcbb6f7fa8e0cff3e970acb828
Unknown <malafar94@gmail.com>
2021-02-27 20:58:42 +0100

    Base ABB y nodos
    Base de generación de ABB y de los nodos con su información.

---
+++ Ygdra/Assets/Ygdra/Pruebas/Manu/Scripts/ABB.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABB {
+
+    public Nodo raiz;
+    private int _lastIndex = 0;
+
+    public ABB() {
+        raiz = null;
+    }
+
+    public ABB(string rutaFichero) {
+        // Por determinar
+    }
+
+    public void insertar(bool decision, int indexPadre) {
+        if (decision) {
+            if (raiz == null) {
+                raiz = new Nodo();
+                addDosHijos(raiz);
+            } else {
+                Nodo recorrer = buscarNodo(indexPadre);
+                addDosHijos(recorrer);
+            }
+        } else {
+            if (raiz == null) {
+                raiz = new Nodo();
+                addUnHijo(raiz);
+            } else {
+                Nodo recorrer = buscarNodo(indexPadre);
+                addUnHijo(recorrer);
+            }
+        }
+    }
+
+    private void addDosHijos(Nodo padre) {
+        padre.addHi();
+        padre.getHi().incrementID(_lastIndex);
+        _lastIndex++;
+        padre.addHijoToList(_lastIndex);
+
+        padre.addHd();
+        padre.getHd().incrementID(_lastIndex);
+        _lastIndex++;
+        padre.addHijoToList(_lastIndex);
+    }
+
+    private void addUnHijo(Nodo padre) {
+        padre.addHi();
+        padre.getHi().incrementID(_lastIndex);
+        _lastIndex++;
+        padre.addHijoToList(_lastIndex);
+    }
+
+    private Nodo buscarNodo(int indexPadre) {
+        Nodo recorrer = raiz;
+
+        while (recorrer.getID() != indexPadre) {
+            if (recorrer.getHi().hijoEnRama(indexPadre)) {
+                recorrer = recorrer.getHi();
+            } else {
+                recorrer = recorrer.getHd();
+            }
+        }
+
+        return recorrer;
+    }
+
+    public void ajustarPagina(int indexNodo, string texto, Sprite ilustracion) {
+        Nodo nodo = buscarNodo(indexNodo);
+
+        nodo.setTexto(texto);
+        nodo.setIlustracion(ilustracion);
+    }
+}
---
+++ Ygdra/Assets/Ygdra/Pruebas/Manu/Scripts/Nodo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodo {
+    private Nodo _hi;
+    private Nodo _hd;
+    private int _id;
+    private List<int> _nodosHijos;
+    private string _texto;
+    private Sprite _ilustracion;
+
+    public Nodo() {
+        _hi = null;
+        _hd = null;
+        _id = 0;
+        _texto = "";
+        _ilustracion = null;
+    }
+
+    public void addHi() {
+        _hi = new Nodo();
+    }
+
+    public Nodo getHi() {
+        return _hi;
+    }
+
+    public void addHd() {
+        _hd = new Nodo();
+    }
+
+    public Nodo getHd() {
+        return _hd;
+    }
+
+    public int getID() {
+        return _id;
+    }
+
+    public void incrementID(int idAnterior) {
+        _id = idAnterior + 1;
+    }
+
+    public void addHijoToList(int indexHijo) {
+        _nodosHijos.Add(indexHijo);
+    }
+
+    public bool hijoEnRama(int indexHijo) {
+        return _nodosHijos.Contains(indexHijo);
+    }
+
+    public void setTexto(string cadena) {
+        _texto = cadena;
+    }
+
+    public void setIlustracion(Sprite imagen) {
+        _ilustracion = imagen;
+    }
+}
